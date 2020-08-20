f2f1eb0671cff08aee44b18294c0eb88bfe17a09
dudeitssm <36095297+dudeitssm@users.noreply.github.com>
2020-08-20 18:35:32 +0000

    Update Cursor.ino

--- ControLeo2/examples/Cursor/Cursor.ino
+++ ControLeo2/examples/Cursor/Cursor.ino
@@ -17,7 +17,9 @@
  by David A. Mellis
  modified 21 October 2014 to work with ControLeo2
  by Peter Easton
- 
+ modified 20 August 2020 for DIY Toner Transfer Etched Copperclad Build
+ by dudeitssm (https://github.com/dudeitssm)
+
  http://www.arduino.cc/en/Tutorial/LiquidCrystal
  */
 
@@ -33,8 +35,8 @@ void setup() {
   pinMode(CONTROLEO_BUTTON_TOP_PIN, INPUT_PULLUP);
   pinMode(CONTROLEO_BUTTON_BOTTOM_PIN, INPUT_PULLUP);
   // Set the relays as outputs and turn them off
-  // The relay outputs are on D4 to D7 (4 outputs)
-  for (int i=4; i<8; i++) {
+  // The relay outputs are on D0 to D2 (3 outputs)
+  for (int i=0; i<3; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
   }
